ce1822865f5971f7db28568fd0de2509fb7060f5
Jake <linweichen08@gmail.com>
2020-09-22 08:35:01 +1000

    Delete button tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -4,29 +4,5 @@ void main() => runApp(MaterialApp(
   title: 'Flutter Material App Tutorial',
   home: MyButton(),
 ));
-class MyButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        print('MyButton was tapped!');
-      },
-      child: Container(
-        height: 36.0,
-        padding: const EdgeInsets.all(8.0),
-        margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5.0),
-          color: Colors.lightBlue,
-        ),
-        child: Center(
-          child: Text('Engage',
-          style: TextStyle(
-            fontSize: 65.0,
-            color: Colors.black
-          ) ),
-        ),
-      ),
-    );
-  }
-}
+
+
