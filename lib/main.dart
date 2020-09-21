bf2031091c5cbdbcabf4d461567a9f170d599056
Jake <linweichen08@gmail.com>
2020-09-21 21:54:53 +1000

    Button Tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -2,37 +2,27 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
   title: 'Flutter Material App Tutorial',
-  home: TutorialHome(),
+  home: MyButton(),
 ));
-
-class TutorialHome extends StatelessWidget {
+class MyButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigation menu',
-          onPressed: null,
+    return GestureDetector(
+      onTap: () {
+        print('MyButton was tapped!');
+      },
+      child: Container(
+        height: 36.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Colors.lightGreen[500],
         ),
-        title: Text('Example title'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          )
-        ],
-      ),
-      body: Center(
-        child: Text('Hello World'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Add',
-        child: Icon(Icons.add),
-        onPressed: null,
+        child: Center(
+          child: Text('Engage'),
+        ),
       ),
     );
   }
 }
-
