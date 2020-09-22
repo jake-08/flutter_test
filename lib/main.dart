fd5f2c44ae15a2453202400a154b5a7c8fcd74cc
Jake <linweichen08@gmail.com>
2020-09-22 11:17:23 +1000

    Oeschinen Lake Tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -7,27 +7,102 @@ void main() => runApp(MaterialApp(
       appBar: AppBar(
         title: Text('Flutter layout demo'),
       ),
-      body: Center(
-        child: Container(
-          margin: EdgeInsets.fromLTRB(0, 40, 0, 30),
-          height: 600,
-          child: Card(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  width: 400,
-                  child: leftColumn,
-                ),
-                //Image.network('https://www.thespruceeats.com/thmb/Kco8yJVmHa7sh0z5sjxUeO7_Cwo=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/australian-pavlova-recipe-256101-hero-01-f0bd5d2a3f0b4ec383fe8d16b8600bc9.jpg')
-              ],
-            ),
+      body: Column(
+        children: [
+          Image.asset(
+            'images/lake.jpg',
+            width: 600,
+            height: 240,
+            fit: BoxFit.cover,
           ),
-        )
-      ),
+          titleSection,
+          buttonSelection,
+          textSection
+        ],
+      )
     )
 ));
 
+var titleSection = Container(
+  padding: EdgeInsets.all(32.0),
+  child: Row(
+    children: [
+      Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: Text(
+                'Oeschinen Lake Campground',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ),
+            Text(
+              'Kandersteg, Switerland',
+              style: TextStyle(
+                color: Colors.grey[500]
+              ),
+            )
+          ],
+        ),
+      ),
+      Icon(
+        Icons.star,
+        color: Colors.red[500],
+      ),
+      SizedBox(width: 5,),
+      Text('41')
+    ],
+  )
+);
 
+Color color = Colors.lightBlue[700];
 
+Widget buttonSelection = Container(
+  child: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      _buildButtonColumn(color, Icons.call, 'CALL'),
+      _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+      _buildButtonColumn(color, Icons.share, 'SHARE')
+    ],
+  ),
+);
+
+Column _buildButtonColumn(Color color, IconData icon, String label){
+  return Column(
+    mainAxisSize: MainAxisSize.min,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Icon(icon, color: color),
+      Container(
+        margin: const EdgeInsets.only(top: 8),
+        child: Text(
+          label,
+          style: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+            color: color
+          ),
+        )
+      )
+    ],
+  );
+}
+
+Widget textSection = Container(
+  padding: const EdgeInsets.all(32),
+  child: Text(
+    'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+    softWrap: true,
+  ),
+);
 
